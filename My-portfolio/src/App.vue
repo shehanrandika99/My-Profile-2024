e04efeed2b3fde2857269476da6c7a6878d86909
shehanrandika99 <agsrandika02@gmail.com>
2024-06-15 13:34:19 +0530

    Created Vue Js Project Template

---
+++ My-portfolio/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <router-view />
+</template>
+
+<style scoped></style>
---
+++ My-portfolio/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <!-- Component template -->
+       <p class="text-gray-500 text-4xl text-center"> HelloWorld This Home</p>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+</script>
+
+<style scoped>
+/* Component styles */
+</style>
