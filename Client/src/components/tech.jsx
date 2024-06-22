8c729e78a72d7c6d7094cf884d77fa5b125df431
shehanrandika99 <agsrandika02@gmail.com>
2024-06-22 12:58:40 +0530

    added new component and change some data

---
+++ Client/src/components/tech.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ProjectsController from '../controllers/ProjectsController';
+function tech() {
+  return (
+    <div>
+         <div className='w-auto h-auto border rounded-xl bg-white text-gray-600 flex items-center justify-center  hover:bg-slate-500 hover:text-black'>{tech}</div>
+    </div>
+  )
+}
+
+export default tech
--- Client/src/models/cardData.js
+++ Client/src/models/cardData.js
@@ -2,28 +2,39 @@
 
 const cardData = [
     {
-        title: 'Project 1',
+        title: 'Color adjustment tool for color-blind people',
         imageUrl: 'url-to-image-1',
-        description: 'Description for project 1',
-        buttonText: 'Learn More'
+        description: 'This project aims to assist individuals with color vision deficiency by providing a comprehensive suite of tools, including the Ishihara Color Blindness Identification System, a color correction web extension, a color correction image upload feature, and a color vision simulation web extension. Leveraging advanced algorithms and user-friendly interfaces, this system offers an inclusive experience for color-blind users to perceive and interact with digital content effectively.',
+        buttonText: 'View on Github',
+        status:'public',
+        url:'https://github.com/shehanrandika99/FInal-Year-Project-2023-2024.git',
+        tech: ['React', 'Python', 'Flask', 'TensorFlow', 'JavaScript','Node js', 'web extensions','AWS','Native Java Mobile App']
     },
     {
-        title: 'Project 2',
-        imageUrl: 'url-to-image-2',
-        description: 'Description for project 2',
-        buttonText: 'Learn More'
+        title: 'Virtual Room Planning System - HCI',
+        imageUrl: '..',
+        description: 'This is a Virtual Room Planning System that allows users and designers / developers to design their own rooms in a Furniture Showroom. The system provides various features such as Feature Selection Dashboard, Design items selection dashboard, User Profile, Place Model and many more features.This for used JOGL, Java Swing, Java AWT, Java desktop app devlopment also using java 3D jmonkey engine2',
+        buttonText: 'View On Github',
+        url:'a href={url} ',
+        status:'private',
+
+        tech: ['Java Swing', 'Jmonkey', 'Figma UI']  // Add technologies for this project
     },
     {
-        title: 'Project 3',
+        title: 'Cinnamon Red Hotel Room Booking System',
         imageUrl: 'url-to-image-3',
-        description: 'Description for project 3',
-        buttonText: 'Learn More'
+        description: 'This is a Hotel Room Booking System that allows users to book rooms in a hotel. The system provides various features such as user registration and login room availability checking room booking and cancellation payment processing and user profile management.',
+        buttonText: 'View on Github',
+        status:'public',
+        url:'https://github.com/ravindu0823/Hotel-Room-Booking-System.git',
+        tech: ['Vite','MERN','Tailwind CSS','Github pipe lines','Docker','Digital Ocean','PayPal Sandbox','Nodemailer']  // Add technologies for this project
     },
     {
         title: 'Project 4',
         imageUrl: 'url-to-image-4',
         description: 'Description for project 4',
-        buttonText: 'Learn More'
+        buttonText: 'Learn More',
+        tech: ['C#', '.NET', 'SQL']  // Add technologies for this project
     },
     // Add more project data as needed
 ];
