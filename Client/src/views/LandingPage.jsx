7dadd51516b0d98b647cf2070aa6e75f0926b2e6
shehanrandika99 <agsrandika02@gmail.com>
2024-06-23 16:01:14 +0530

    chore: Add react-responsive-video dependency and update HeroComponent

--- Client/src/views/LandingPage.jsx
+++ Client/src/views/LandingPage.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import NavbarComponent from '../components/NavbarComponent';
-import HomePage from './HomePage';
 import HeroComponent from '../components/HeroComponent';
 import HeroComponent2 from '../components/HeroComponent2';
 import '../styles/Cursor.css'; // Import the CSS file for cursor styling
@@ -10,6 +9,7 @@ import ProjectsComponents from '../components/ProjectsComponents';
 import ContactMeComponent from '../components/ContactMeComponent';
 import EducationComponent from '../components/EducationComponent';
 import ExperiencesComponent from '../components/ExperiencesComponent';
+import FooterComponent from '../components/FooterComponent';
 
 function LandingPage() {
     const [position, setPosition] = useState({ x: 0, y: 0 });
@@ -28,9 +28,13 @@ function LandingPage() {
     const contactmeRef = React.useRef(null);
     const educationRef = React.useRef(null);
     const experienceRef = React.useRef(null);
+    const footerRef = React.useRef(null);
 
     return (
-        <div onMouseMove={handleMouseMove} className='bg-gradient-to-r from-[#ffffff] via-[#e4efde] to-[#fff]' >
+        <div 
+            onMouseMove={handleMouseMove} 
+            className="bg-gradient-to-r from-[#ffffff] via-[#e4efde] to-[#fff] animate-gradient-xy min-h-screen"
+        >
             <NavbarComponent
                 scrollToAboutMe={() => scrollToRef(aboutMeRef)}
                 scrollToSkills={() => scrollToRef(skillsRef)}
@@ -38,6 +42,7 @@ function LandingPage() {
                 scrollToContactMe={() => scrollToRef(contactmeRef)}
                 scrollToEducation={() => scrollToRef(educationRef)}
                 scrollToExperience={() => scrollToRef(experienceRef)}
+                scrollToFooter={() => scrollToRef(footerRef)}
             />
             <div className="mt-20" ref={aboutMeRef}>
                 <HeroComponent />
@@ -53,13 +58,16 @@ function LandingPage() {
             {/* <div className="custom-cursor" style={{ left: `${position.x}px`, top: `${position.y}px` }}></div> */}
   
             <div ref={educationRef}>
-                <EducationComponent/>
+                <EducationComponent />
             </div>
             <div ref={experienceRef}>
-                <ExperiencesComponent/>
+                <ExperiencesComponent />
             </div>
             <div ref={contactmeRef}>
-                <ContactMeComponent/>
+                <ContactMeComponent />
+            </div>
+            <div ref={footerRef}>
+                <FooterComponent />
             </div>
         </div>
     );
